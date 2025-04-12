c62864f0c197db3218c5918f48cba27b1687cfa4
p.k.filimonov <p.k.filimonov@gmail.com>
2025-04-12 12:20:43 -0300

    Small import fix in tests

--- test/Oikos.t.sol
+++ test/Oikos.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../src/Oikos.sol";
+import "src/Oikos.sol";
 
 /// @title OikosToken Unit Tests
 /// @notice Full test suite for the Oikos contract.
